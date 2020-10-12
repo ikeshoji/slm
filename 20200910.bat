a43a6f905c37ca9bcbec27ca4632a8cccf577dcd
Toshi-Taka Ikeshoji <ikeshoj.tt@hiro.kindai.ac.jp>
2020-10-12 23:37:51 +0900

    Add laser profile with 12.5um grids

---
+++ 20200910.bat
@@ -0,0 +1,116 @@
+SET fnm=20200910
+SET fld=D:\20200910
+
+
+SET sfx=a1
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=a2
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=a3
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=a4
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=b1
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=b2
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=b3
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=b4
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=c1
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=c2
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=c3
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=c4
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=d1
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=d2
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=d3
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
+SET sfx=d4
+SET jbn=%fnm%%sfx%
+SET inp=%fld%\%fnm%%sfx%.inp
+SET out=%fld%\%fnm%%sfx%.out
+"C:\Program Files\ANSYS Inc\v201\ansys\bin\winx64\MAPDL.exe" -b -g -p aa_r -dis -mpi INTELMPI -np 16 -lch -dir %fld% -j %jbn% -i %inp% -o %out% -s read -m 10240 -db 5120 -l en-us -t -d win32
+
+
